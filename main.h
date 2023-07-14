eecb7f2c9e9bd7c9c866f90ce36e165b2b7d924f
BluoCaroot <assem.akl2004@gmail.com>
2023-07-14 13:00:29 -0700

    added .gitignore and main.h

---
+++ main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <errno.h>
+
+#endif
